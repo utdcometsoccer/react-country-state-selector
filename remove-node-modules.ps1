1be80a08e26eac81152a05cf62cdf4807cdc4bb7
Idaho  Edokpayi <idaho@edokpayi.com>
2025-09-01 12:19:26 -0600

    updates

--- remove-node-modules.ps1
+++ remove-node-modules.ps1
@@ -3,3 +3,14 @@ Get-ChildItem -Path . -Directory -Recurse -Filter node_modules | ForEach-Object 
     git rm -r --cached $_.FullName
     Write-Host "Removed from git: $($_.FullName)"
 }
+
+# Remove all dist folders from git tracking
+Get-ChildItem -Path . -Directory -Recurse -Filter dist | ForEach-Object {
+    git rm -r --cached $_.FullName
+    Write-Host "Removed from git: $($_.FullName)"
+}
+
+Get-ChildItem -Path . -Directory -Recurse -Filter coverage | ForEach-Object {
+    git rm -r --cached $_.FullName
+    Write-Host "Removed from git: $($_.FullName)"
+}
